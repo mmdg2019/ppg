bd40cb05b78f6a3ae87180bb670bd58c72b2f1ce
Bo Bo Oo <bobooo.1249@gmail.com>
2021-10-01 09:50:40 +0000

    Add calculation function for sold count

--- popular_reports/models/.ipynb_checkpoints/sale_target-checkpoint.py
+++ popular_reports/models/.ipynb_checkpoints/sale_target-checkpoint.py
@@ -19,7 +19,7 @@ class sales_target(models.Model):
     end_date = fields.Date(string='End Date', required=True)
     company_id = fields.Many2one('res.company', 'Company', index=True, ondelete='cascade', default=lambda self: self.env.user.company_id.id)
 #                                  ,default= _compute_company_id)
-    sale_target_line_ids = fields.One2many('popular_reports.sale_target.line', 'sale_target_id' ,'Product List', auto_join=True)
+    sale_target_line_ids = fields.One2many('popular_reports.sale_target.line', 'sale_target_id' ,'Product List', auto_join=True, copy=True)
     sale_target_line_ids_count = fields.Integer(string='Sales Target Line Counts', compute = '_compute_sales_target_line')
     complete_name = fields.Char(
         'Complete Name', compute='_compute_complete_name',
@@ -41,6 +41,13 @@ class sales_target(models.Model):
         dic = {}
         for x in results: dic[x['sale_target_id'][0]] = x['sale_target_id_count']
         for record in self: record['sale_target_line_ids_count'] = dic.get(record.id, 0)
+    
+#     @api.depends('sale_target_line_ids')
+#     def _default_sale_target_line_ids(self):
+#         results = self.env['product.template'].search([],order='display_name asc')
+#         sale_target_line = self.env['popular_reports.sale_target.line'].browse()
+#         for record in self:
+#             raise Warning(record['id'])
 
 #     @api.depends()
 #     def _compute_complete_name(self):
@@ -53,13 +60,21 @@ class sales_target_line(models.Model):
     _rec_name = 'product_id'
     _order = 'product_id'
 #     date = fields.Date(string='Date')
-    product_id = fields.Many2one('product.template', string='Product', required=True)
+    product_id = fields.Many2one('product.product', string='Product', required=True)
     prouct_uom_id = fields.Char(related='product_id.uom_name', string='Product', store=True)
-    ttl_sold_count = fields.Float(string='Total Sold Count', store=True)
+    ttl_sold_count = fields.Float(string='Total Sold Count', store=True, compute='_compute_ttl_sold_count')
     sale_target_number = fields.Float(string='Sales Target Number', required=True, default = 0.0)
     company_id = fields.Many2one('res.company', 'Company', index=True, ondelete='cascade', default=lambda self: self.env.user.company_id.id)
 #     odoo.fields.Reference
 #     company_id = fields.Many2one('res.company', 'Company', index=True, ondelete='cascade')
-    sale_target_id = fields.Many2one('popular_reports.sale_target', string='Sales Target Reference', required=True, ondelete='cascade', index=True, copy=False)
-    
-#     sale_target_number
+    sale_target_id = fields.Many2one('popular_reports.sale_target', string='Sales Target Reference', required=True, ondelete='cascade', index=True)
+    @api.depends('product_id', 'sale_target_id', 'sale_target_number')
+    def _compute_ttl_sold_count(self):
+        for temp in self:
+            if temp.sale_target_id.id:
+                result_sale_target = self.env['popular_reports.sale_target'].search([('id','=',temp.sale_target_id.id)],order='display_name asc', limit=1)
+                if len(result_sale_target) > 0:
+                    result_invoice_report = self.env['account.invoice.report'].search([('product_id','=',temp.product_id.id),('invoice_date', '>=',result_sale_target.start_date),('invoice_date', '<=',result_sale_target.end_date)])
+                    if len(result_invoice_report) > 0:
+                        temp.ttl_sold_count = sum(result_invoice_report.mapped('quantity'))
+#                     rst_sales_report = self.env['account.invoice.report'].read_group([('product_id','=',temp.product_id.id)], fields=['product_id','quantity'], groupby=['product_id'],lazy=False)
