8d93473adb87b7deace3251fdde94b07927b2e23
twetaoo <tweta.oo@digipowermm.com>
2023-02-03 12:29:56 +0630

    account_ext: color invoice lines based on due status scheduler and action

--- account_ext/__manifest__.py
+++ account_ext/__manifest__.py
@@ -4,16 +4,17 @@
     'version': '1.0.1',
     'author': 'DIGI POWER',
     'license': 'AGPL-3',
-    'category': 'Account Reports',
+    'category': 'Accounting/Accounting',
     'website': 'https://www.digipowermm.com/',
     'description': """
 
-General Ledger Report Customization
+Account Customization
     """,      
     'depends': ['base', 'account_reports', 'account','sale'],    
     'data': [
         # 'security/ir.model.access.csv',
-        'security/security.xml',        
+        'security/security.xml',      
+        'data/scheduler_update_invoice_due_state_data.xml',  
         'views/res_partner_view.xml',
         'views/account_move_view.xml',
         'views/sale_order_view.xml',
--- account_ext/models/account_move.py
+++ account_ext/models/account_move.py
@@ -1,17 +1,12 @@
-from odoo.tools.misc import DEFAULT_SERVER_DATETIME_FORMAT
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 from datetime import datetime, date, timedelta
-import json
-import datetime
-import pytz
-import io
+from dateutil.relativedelta import relativedelta
 from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
-from odoo.http import request
-from odoo.tools import date_utils
-import pandas as pd
+
 
 class AccountMove(models.Model):
-
     _inherit = 'account.move'
 
     @api.depends('user_id')
@@ -26,3 +21,66 @@ class AccountMove(models.Model):
     
     check_user=fields.Boolean(string='user', compute='_compute_user_check')   
         
+
+    invoice_due_state = fields.Selection([
+        ('no_due', 'No Due'),
+        ('first_due', 'First Due'), 
+        ('second_due', 'Second Due'),
+        ('third_due', 'Third Due')], 
+        string='Due Status', readonly=True)
+
+    # calculate invoice due dates
+    def calculate_invoice_due_date(self, date_ref):      
+        for line in self.invoice_payment_term_id.line_ids:
+            inv_due_date = fields.Date.from_string(date_ref)
+            if line.option == 'day_after_invoice_date':
+                inv_due_date += relativedelta(days=line.days)
+                if line.day_of_the_month > 0:
+                    months_delta = (line.day_of_the_month < inv_due_date.day) and 1 or 0
+                    inv_due_date += relativedelta(day=line.day_of_the_month, months=months_delta)
+            elif line.option == 'after_invoice_month':
+                next_first_date = inv_due_date + relativedelta(day=1, months=1)  # Getting 1st of next month
+                inv_due_date = next_first_date + relativedelta(days=line.days - 1)
+            elif line.option == 'day_following_month':
+                inv_due_date += relativedelta(day=line.days, months=1)
+            elif line.option == 'day_current_month':
+                inv_due_date += relativedelta(day=line.days, months=0)
+        return inv_due_date
+
+    # update invoice due state
+    def update_invoice_due_state(self):
+        if self.type == 'out_invoice' and self.state not in ('draft', 'cancel') and self.invoice_payment_state != 'paid' and self.invoice_payment_term_id:    
+            today = fields.Date.context_today(self)
+            if today <= self.invoice_date_due:
+                self.invoice_due_state = 'no_due'
+            else:
+                second_due_date = self.calculate_invoice_due_date(self.invoice_date_due)
+                third_due_date = self.calculate_invoice_due_date(second_due_date)
+                if today > self.invoice_date_due and today <= second_due_date:
+                    self.invoice_due_state = 'first_due'
+                elif today > second_due_date and today <= third_due_date:
+                    self.invoice_due_state = 'second_due'
+                elif today > third_due_date:
+                    self.invoice_due_state = 'third_due'
+        else:
+            self.invoice_due_state = 'no_due'
+
+    # update invoice due state scheduler; runs once a day
+    def _cron_update_invoice_due_state(self):
+        # set invoice_due_state ("No Due") for paid invoices or those with draft/cancel state or no-payment-terms 
+        no_due_invoices = self.search([            
+            ('type', '=', 'out_invoice'), 
+            ('create_date', '>=', datetime(2023, 2, 1)), 
+            '|', '|', ('state', 'in', ('draft', 'cancel')), ('invoice_payment_state', '=', 'paid'), ('invoice_payment_term_id', '=', False)])
+        no_due_invoices.write({'invoice_due_state': 'no_due'})
+
+        # set invoice_due_state for other invoices
+        other_invoices = self.search([
+            ('type', '=', 'out_invoice'), 
+            ('create_date', '>=', datetime(2023, 2, 1)),
+            ('state', 'not in', ('draft', 'cancel')), 
+            ('invoice_payment_state', '!=', 'paid'),
+            ('invoice_payment_term_id', '!=', False)])
+        for invoice in other_invoices:
+            invoice.update_invoice_due_state()       
+    
