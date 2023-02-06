14dcc5ce46a591887ea380edcc3f1faa4c66c326
Yair Thu Aung <yethu.aung@digipowermm.com>
2023-02-06 21:05:02 +0630

    change string in partner views

--- account_ext/__manifest__.py
+++ account_ext/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Account Customization',
-    'version': '1.1.3',
+    'version': '2.1.3',
     'author': 'DIGI POWER',
     'license': 'AGPL-3',
     'category': 'Accounting/Accounting',
