46927af64417ac7247959690366415d6043189c0
popodigipower <myintmyint.aye@digipowermm.com>
2023-09-08 16:54:13 +0630

    add field for so block customer in Customer

--- ppg_credit_permission/models/res_partner_inherit.py
+++ ppg_credit_permission/models/res_partner_inherit.py
@@ -6,4 +6,5 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     
-    show_credit_due_access = fields.Boolean(string="Credit Due")  
+    show_credit_due_access = fields.Boolean(string="Credit Due")
+    so_block_customer = fields.Boolean(string="SO Block Customer")  
