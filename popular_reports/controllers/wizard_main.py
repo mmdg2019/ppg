8f37aec3c227ef5ff6a95dfa2484588937c38973
Kyi-Maw <kyikyi.maw@digipowermm.com>
2024-01-23 09:27:10 +0630

    popular_reports: change move_lines to move_ids in stocking.picking

--- popular_reports/controllers/wizard_main.py
+++ popular_reports/controllers/wizard_main.py
@@ -1105,7 +1105,7 @@ class edit_report_stock_transfer_dtl_info(models.AbstractModel):
         
         items = []
         temp = []
-        products = list(set(docs.mapped('move_lines.product_id')))
+        products = list(set(docs.mapped('move_ids.product_id')))
         picking_types = list(set(docs.mapped('picking_type_id')))
         if data['user_ids']:
             docs = docs.filtered(lambda r: r.partner_id.id in data['user_ids'])
@@ -1118,8 +1118,8 @@ class edit_report_stock_transfer_dtl_info(models.AbstractModel):
         total_done = 0
         for location in locations:
             for product in products:
-                total_demand = sum(table_line.product_uom_qty for doc in docs.filtered(lambda r: r.location_id == location) for table_line in doc.move_lines.filtered(lambda r: r.product_id == product))
-                total_done = sum(table_line.quantity_done for doc in docs.filtered(lambda r: r.location_id == location) for table_line in doc.move_lines.filtered(lambda r: r.product_id == product))
+                total_demand = sum(table_line.product_uom_qty for doc in docs.filtered(lambda r: r.location_id == location) for table_line in doc.move_ids.filtered(lambda r: r.product_id == product))
+                total_done = sum(table_line.quantity_done for doc in docs.filtered(lambda r: r.location_id == location) for table_line in doc.move_ids.filtered(lambda r: r.product_id == product))
                 temp.append({'location':location, 'product':product, 'ttl_done':total_done, 'ttl_demand':total_demand})
         return {
             'filter_post_stock': data['filter_post_stock'],
