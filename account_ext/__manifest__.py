43d17a4ff67f477a6df5eb84bea62565d4c712df
popodigipower <myintmyint.aye@digipowermm.com>
2024-01-30 11:04:49 +0630

    conflict for account_ext

--- account_ext/__manifest__.py
+++ account_ext/__manifest__.py
@@ -12,7 +12,7 @@ Account Customization
     """,      
     'depends': ['base', 'account_reports', 'account','sale', 'ppg_credit_permission'],    
     'data': [
-        'security/security.xml', 
+        'security/security.xml',   
         'security/ir.model.access.csv',
         'data/scheduler_update_invoice_due_state_data.xml',  
         # 'views/res_partner_view.xml',
