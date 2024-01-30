f5ec2e926ca67daa03368e92804c19734941af64
popodigipower <myintmyint.aye@digipowermm.com>
2024-01-30 11:04:49 +0630

    update manifest file declare for security

--- account_ext/__manifest__.py
+++ account_ext/__manifest__.py
@@ -12,8 +12,8 @@ Account Customization
     """,      
     'depends': ['base', 'account_reports', 'account','sale', 'ppg_credit_permission'],    
     'data': [
+        'security/security.xml',   
         'security/ir.model.access.csv',
-        'security/security.xml',      
         'data/scheduler_update_invoice_due_state_data.xml',  
         # 'views/res_partner_view.xml',
         'views/account_move_view.xml',
