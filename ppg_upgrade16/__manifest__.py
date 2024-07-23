384b481d06d703c8f34a0ea7331678b35447b1c9
popodigipower <myintmyint.aye@digipowermm.com>
2024-07-23 19:04:09 +0630

    resequence for payment and visible for operation type in stock

---
+++ ppg_upgrade16/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ppg_upgrade16",
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+    'description': """
+        Long description of module's purpose
+    """,
+    'author': "DiGi Power",
+    'website': "https://www.digipower.com.mm",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'stock','account'],  # Ensure 'stock' module is added since you are inheriting stock views
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/stock_picking_inherit_view.xml',  # Updated to include the correct view file
+        'views/account_payment_inherit.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
