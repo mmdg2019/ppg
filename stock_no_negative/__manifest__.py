ab4c9460df4c8366637566a3284811c4560943e4
popodigipower <myintmyint.aye@digipowermm.com>
2024-08-07 13:17:35 +0630

    repush:stock no negative

--- stock_no_negative/__manifest__.py
+++ stock_no_negative/__manifest__.py
@@ -2,7 +2,6 @@
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-
 {
     "name": "Stock Disallow Negative",
     "version": "16.0.1.0.2",
