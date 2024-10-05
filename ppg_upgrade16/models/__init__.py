5d41f0ec712141624a48104ba4fa43191606c2d4
popodigipower <myintmyint.aye@digipowermm.com>
2024-10-05 08:41:10 +0630

    change journal entry sequence depends on payment's journal

--- ppg_upgrade16/models/__init__.py
+++ ppg_upgrade16/models/__init__.py
@@ -5,3 +5,4 @@ from . import account_move_inherit
 from . import mrp_production_inherit
 from . import stock_move_inherit
 # from . import account_move_line_inherit
+from . import sale_order_line_inherit
