bb2face99ff4769b53bf2a9a629247b888d77326
popodigipower <myintmyint.aye@digipowermm.com>
2024-10-14 10:50:27 +0630

    13 coa current asset

--- ppg_upgrade16/models/__init__.py
+++ ppg_upgrade16/models/__init__.py
@@ -6,3 +6,4 @@ from . import mrp_production_inherit
 from . import stock_move_inherit
 # from . import account_move_line_inherit
 from . import sale_order_line_inherit
+from . import account_account_inherit
