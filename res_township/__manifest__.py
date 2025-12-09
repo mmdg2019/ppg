44ce14457caaba5f4b531c4ec9488a0788cafda7
Myo Zin Oo <myo910967@gmail.com>
2025-12-09 13:21:00 +0630

    add new module res_township

---
+++ res_township/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name": "PPG Township",
+    "version": "1.0",
+    "category": "Custom",
+    "summary": "Manage Township Information",
+    "description": """
+        This module provides functionality to manage township information, including creating, updating, and deleting townships.
+    """,
+    "author": "Your Name",
+    "website": "https://www.example.com",
+    "depends": [
+        "base",
+        "base_address_extended",
+        "sale",
+    ],
+    "data": [
+        'security/ir.model.access.csv',
+        "views/res_township_views.xml",
+        "views/res_township_menus.xml",
+        'views/res_partner_views.xml',
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
