33da76b044e95b9472cde1382b75caa41dc9d37f
twetaoo <tweta.oo@digipowermm.com>
2025-12-22 10:24:05 +0630

    account_ext: remove price zero check of invoice and move price zero check of SO from confirm button click to create/update

--- account_ext/models/sale_order.py
+++ account_ext/models/sale_order.py
@@ -34,8 +34,6 @@ class SaleOrder(models.Model):
                 if record.partner_id.so_block_customer and not self.env.user.has_group('ppg_credit_permission.group_credit_permission'):
                 # if due_invoice_count > 0 and not self.env.user.has_group('ppg_credit_permission.group_credit_permission'):
                     raise AccessError(_("You don't have the access rights to sell to customers with overdue invoices."))
-            if not all(record.order_line.mapped('price_unit')):
-                raise UserError(_("Order lines should not have products with ZERO unit price. Please check!"))
         return super(SaleOrder, self).action_confirm()
 
     @api.model
@@ -65,3 +63,11 @@ class SaleOrder(models.Model):
                     raise AccessError(_("You don't have the access rights to sell to customers with overdue invoices."))
         return super(SaleOrder, self).write(values)
     
+    # check for products with zero original sales price on pricelist (for changes made via UI)
+    @api.onchange('order_line')
+    def _onchange_product_price_zero_check(self):
+        if not self.x_studio_editing_price_status:
+            for ol in self.order_line.filtered(lambda x: x.product_id.id not in (2350, 2351)): # don't need to check "Other Charges" and "Special Discount"
+                price_list_lines = self.pricelist_id.item_ids.filtered(lambda r: r.product_tmpl_id.product_variant_id.id == ol.product_id.id)
+                if price_list_lines and price_list_lines[0].x_studio_original_sales_price == 0.0:
+                    raise ValidationError(_('The product "%s" has price ZERO.', ol.product_id.display_name))
