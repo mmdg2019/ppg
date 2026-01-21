094972588286ea6cc258590a2d543b4f861bcf12
Myo Zin Oo <myo910967@gmail.com>
2026-01-21 14:16:25 +0630

    add new field(car_size,car_ton,car_length,car_width,car_height) in account_move

--- car_way_management/models/account_move.py
+++ car_way_management/models/account_move.py
@@ -8,3 +8,28 @@ class AccountMove(models.Model):
         string='Car Number',
         readonly=True
     )
+    car_size = fields.Char(
+        string='Car Size',
+        related='car_number_id.car_size',
+        readonly=True
+    )
+    car_ton = fields.Char(
+        string='Car Ton',
+        related='car_number_id.car_ton',
+        readonly=True   
+    )
+    car_length = fields.Char(
+        string='Car Length',
+        related='car_number_id.car_length',
+        readonly=True
+    )
+    car_width = fields.Char(
+        string='Car Width',
+        related='car_number_id.car_width',  
+        readonly=True
+    )
+    car_height = fields.Char(
+        string='Car Height',
+        related='car_number_id.car_height',
+        readonly=True
+    )
