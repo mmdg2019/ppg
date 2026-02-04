e45088d4c4ca0fc1285d7439f1f32c51b597d078
Yair Thu Aung <yethu.aung@digipowermm.com>
2026-02-04 11:15:18 +0630

    upgrade popular_web_portal

--- popular_web_portal/__manifest__.py
+++ popular_web_portal/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
     'name': "Hide Web Portal Documents",
 
@@ -17,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': "19.0.1.0.0",
 
     # any module necessary for this one to work correctly
     'depends': ['sale','account','portal','purchase'],
