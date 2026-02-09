8b5d4726bacfb0b6bcd3d64fa818f1e818b1bdd2
Yair Thu Aung <yethu.aung@digipowermm.com>
2026-02-09 20:14:54 +0630

    add license

--- mrp_ext/__manifest__.py
+++ mrp_ext/__manifest__.py
@@ -13,6 +13,7 @@ Long description of module's purpose
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
+    'license': 'AGPL-3',
     'category': 'Uncategorized',
     'version': '19.0.1.0.0',
 
