bc0d1a9237570c0bf63e0092b69cd4f9a186d7dc
Yair Thu Aung <yethu.aung@digipowermm.com>
2026-02-11 10:47:04 +0630

    comment off all xml

--- car_way_management/__manifest__.py
+++ car_way_management/__manifest__.py
@@ -13,17 +13,16 @@
         - Group orders by township
         - Export to Excel functionality
     """,
-    
-    "depends": ["base", "sale","res_township","stock","account"],
+    "depends": ["base", "sale", "res_township", "stock", "account"],
     "data": [
         "security/ir.model.access.csv",
         "security/car_number_security.xml",
-        "views/car_number_views.xml",
-        "views/assign_by_township_wizard_views.xml",
-        "views/sale_order_views.xml",
-        "views/car_management_menus.xml",
-        'views/export_car_way_wizard_views.xml',
-        'views/stock_picking_views.xml',
+        # "views/car_number_views.xml",
+        # "views/assign_by_township_wizard_views.xml",
+        # "views/sale_order_views.xml",
+        # "views/car_management_menus.xml",
+        # 'views/export_car_way_wizard_views.xml',
+        # 'views/stock_picking_views.xml',
         # 'views/account_move_views.xml',
     ],
     "installable": True,
