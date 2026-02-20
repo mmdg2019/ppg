d0aff3004615dca5e2d9ffb27ff300d3f748e7b7
twetaoo <tweta.oo@digipowermm.com>
2026-02-20 13:55:51 +0630

    account_ext: solve account.move ZERO debit/credit issue

--- account_ext/models/account_move_line.py
+++ account_ext/models/account_move_line.py
@@ -18,26 +18,26 @@ class AccountMoveLine(models.Model):
         currency_field='company_currency_id', default=0
     )
 
-     #from ppg_upgrade16/account_move_inherit.py
-    @api.onchange('debit')
-    def _inverse_debit(self):
-        pass
+    #  #from ppg_upgrade16/account_move_inherit.py
+    # @api.onchange('debit')
+    # def _inverse_debit(self):
+    #     pass
 
-     #from ppg_upgrade16/account_move_inherit.py
-    @api.onchange('credit')
-    def _inverse_credit(self):
-        pass
+    #  #from ppg_upgrade16/account_move_inherit.py
+    # @api.onchange('credit')
+    # def _inverse_credit(self):
+    #     pass
 
-     #from ppg_upgrade16/account_move_inherit.py
-    @api.depends('balance', 'move_id.is_storno')
-    def _compute_debit_credit(self):
-        pass
+    #  #from ppg_upgrade16/account_move_inherit.py
+    # @api.depends('balance', 'move_id.is_storno')
+    # def _compute_debit_credit(self):
+    #     pass
 
-     #from ppg_upgrade16/account_move_inherit.py
-    @api.depends('debit', 'credit')
-    def _compute_balance(self):
-        for line in self:
-            line.balance = line.debit - line.credit
+    #  #from ppg_upgrade16/account_move_inherit.py
+    # @api.depends('debit', 'credit')
+    # def _compute_balance(self):
+    #     for line in self:
+    #         line.balance = line.debit - line.credit
 
     # sort the invoices to be reconciled by due date, invoice date and currency_id
     def reconcile(self):
