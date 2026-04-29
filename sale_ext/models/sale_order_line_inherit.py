3319dce13d5b7c38635054a217d75947fdafe10d
Kyi-Maw <kyikyi.maw@digipowermm.com>
2026-04-29 10:47:16 +0630

    sale_ext: add unit price (orignal sale price from pricelist) to sale order line

--- sale_ext/models/sale_order_line_inherit.py
+++ sale_ext/models/sale_order_line_inherit.py
@@ -7,6 +7,8 @@ from odoo.exceptions import ValidationError
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'      
 
+    sales_unit_price = fields.Float(string="Sales Unit Price") # original sales price from price list 
+
     @api.depends('product_packaging_id', 'product_uom', 'product_uom_qty')
     def _compute_product_packaging_qty(self):   
         res = super(SaleOrderLine, self)._compute_product_packaging_qty()
@@ -23,17 +25,29 @@ class SaleOrderLine(models.Model):
     @api.depends('product_id', 'product_uom_id', 'product_uom_qty')
     def _compute_price_unit(self):
         super(SaleOrderLine, self)._compute_price_unit()
-        for line in self:
+        for line in self:            
             if line.order_id and not line.order_id.x_studio_editing_price_status and line.product_id and line.product_id.id not in (2350, 2351): # no need to consider "Other Charges" and "Special Discount"
                 product_price_list = line.order_id.pricelist_id.item_ids.filtered(lambda x: x.product_tmpl_id.product_variant_id.id == line.product_id.id)
-                if product_price_list:
+                if product_price_list:                    
                     if line.product_id.uom_id != line.product_uom_id:
                         sales_price = (product_price_list[0].x_studio_original_sales_price / line.product_id.uom_id.factor) * line.product_uom_id.factor
                     else:
                         sales_price = product_price_list[0].x_studio_original_sales_price
                     if sales_price == 0.0:
                         raise ValidationError(_('The product "%s" has price ZERO.', line.product_id.display_name))
+                    line.sales_unit_price = product_price_list[0].x_studio_original_sales_price
                     line.price_unit = sales_price
                     line.discount = product_price_list[0].percent_price
                 else:
                     raise ValidationError(_('No pricelist for the product "%s"!', line.product_id.display_name))
+            elif line.order_id and line.order_id.x_studio_editing_price_status  and line.order_id.locked == False and line.product_id and line.product_id.id not in (2350, 2351): # no need to consider "Other Charges" and "Special Discount"
+                # if line.sales_unit_price != 0:
+                    line.price_unit = line.sales_unit_price * line.product_uom_id.relative_factor
+    
+#    change price_unit (package unit price) according to manually input sales unit price
+    @api.onchange('sales_unit_price')
+    def _onchange_sales_unit_price(self):             
+        for line in self:
+            if line.order_id and line.order_id.x_studio_editing_price_status  and line.order_id.locked == False and line.product_id and line.product_id.id not in (2350, 2351):
+                line.price_unit = line.sales_unit_price * line.product_uom_id.relative_factor   
+
